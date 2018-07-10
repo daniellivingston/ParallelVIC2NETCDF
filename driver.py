729ef75c743665b3527839b7b1a65a6335ba7a47
Daniel Reece Livingston <livingston@lanl.gov>
2018-07-10 16:02:15 -0600

    Working

--- driver.py
+++ driver.py
@@ -4,7 +4,8 @@
 """ CONCURRENCY DRIVER FOR VIC2NETCDF """
 
 from __future__ import print_function
-from multiprocessing import Pool
+from random import randint
+import multiprocessing
 import argparse
 import subprocess
 import datetime
@@ -15,12 +16,9 @@ from tonic import version
 from tonic.models.vic import netcdf2vic, compare_soil_params, grid_params, \
     ncparam2ascii, vic2netcdf
 
-
 # Define global 'constants'
+SEED = randint(1,1e4)
 BASE_CLASS_FILE = os.path.join(os.getcwd(),'control.cfg')
-#NPROCS = 20
-#YEAR_RANGE = [1950,1990]
-
 
 def split_seq(seq, rank):
     '''
@@ -43,35 +41,48 @@ def split_seq(seq, rank):
         newseq.append(seq[int(round(i*splitsize)):int(round((i+1)*splitsize))])
     return newseq
 
-def create_control_files(nprocs,year_range,gcm):
+def create_control_files(args):
     '''
     Generate NPROCS number of control files.
-
-    :param nprocs: number of processors used
-    :type nprocs: int
-    :param year_range: [starting year, ending year]
-    :type year_range: list<int>
-    :param gcm: the GCM name to convert
-    :type gcm: str
     '''
 
+    # Temporary control file directory
+    cfile_dir = os.path.join(os.path.dirname(BASE_CLASS_FILE),'control_files/')
+
+    # Generate year range
+    year_range = [args.year_begin,args.year_end]
+
+    # Check if exists - if not, create
+    if not os.path.exists(cfile_dir):
+        os.mkdir(cfile_dir)
+    if not os.path.exists(args.outdir):
+        print(args.outdir)
+
+    assert isinstance(args.nprocs,int), 'nprocs must be an integer'
+    assert isinstance(args.gcm,str), 'GCM must be a string'
+    assert isinstance(year_range,list), 'year_range must be a list<int,int>'
+
     division = list(range(year_range[0],year_range[1]+1))
-    slices = split_seq(division,nprocs)
+    slices = split_seq(division,args.nprocs)
 
     control_files = []
 
     # Iterate over each processor and generate a relevant control file
-    for i in range(nprocs):
-        proc_file = os.path.join(os.path.dirname(BASE_CLASS_FILE),'control_files/proc_%d.cfg' % i)
+    for i in range(args.nprocs):
+        proc_file = os.path.join(cfile_dir,'%s_%d_proc_%d.cfg' % (args.gcm,SEED,i))
         shutil.copy(BASE_CLASS_FILE,proc_file)
 
-        cmd1 = "sed -i 's/YEARBEGIN/%d/g' %s" % (slices[i][0],proc_file)
-        cmd2 = "sed -i 's/YEAREND/%d/g' %s" % (slices[i][-1],proc_file)
-        cmd3 = "sed -i 's/PROCESSGCM/%s/g' %s" % (gcm,proc_file)
+        # Generate all sed commands
+        cmds = ["sed -i 's/YEARBEGIN/%d/g' %s" % (slices[i][0],proc_file),
+                "sed -i 's/YEAREND/%d/g' %s" % (slices[i][-1],proc_file),
+                "sed -i 's/PROCESSGCM/%s/g' %s" % (args.gcm,proc_file),
+                "sed -i 's,GCMINDIRECTORY,%s,g' %s" % (args.infiles,proc_file),
+                "sed -i 's,GCMOUTDIRECTORY,%s,g' %s" % (args.outdir,proc_file),
+                "sed -i 's,GCMOUTPREFIX,%s,g' %s" % (args.prefix,proc_file)]
 
-        subprocess.check_output(cmd1, shell=True, stderr=subprocess.STDOUT)
-        subprocess.check_output(cmd2, shell=True, stderr=subprocess.STDOUT)
-        subprocess.check_output(cmd3, shell=True, stderr=subprocess.STDOUT)
+        # Run all sed commands
+        for cmd in cmds:
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
 
         control_files.append(proc_file)
 
@@ -96,20 +107,21 @@ def run_config(cfg):
     '''
 
     t0 = time.perf_counter()
-    args = DummyArgs(cfg)
+    fake_args = DummyArgs(cfg)
 
     # Run the program!!
-    vic2netcdf._run(args)
+    vic2netcdf._run(fake_args)
 
     t1 = time.perf_counter()
-    print('SUBPROCESS COMPLETED: %d seconds' % round((t1-t0),3))
+    process = multiprocessing.current_process().name
+    print('%s completed in %d seconds' % (process,round((t1-t0),3)))
 
-arg_help = [
-'''Parallel batch conversion from VIC to netCDF file formats.
+arg_help = ['''Parallel batch conversion from VIC to netCDF file formats.
 Given initial parameters, will convert (in parallel!) requested files to the binary netCDF format.
 
-Uses the big memory mode - be careful how many years and processes you allocate! (20 processors will require approximately 150 GB of RAM - keep an eye on htop - this program WILL slow to a crawl if swap space becomes required!) 
-''',
+Uses the big memory mode - be careful how many years and processes you allocate!
+(20 processors will require approximately 150 GB of RAM. Keep an eye on htop - this program WILL
+slow to a crawl if swap space becomes required!)''',
 'Beginning year of conversions',
 'Ending year of conversions',
 'Name of GCM (i.e., CCSM4)',
@@ -124,7 +136,7 @@ if (__name__ == '__main__'):
     parser = argparse.ArgumentParser(description=arg_help[0])
     parser.add_argument('-y0','--year_begin',type=int,help=arg_help[1],default=1950)
     parser.add_argument('-y1','--year_end',type=int,help=arg_help[2],default=2098)
-    parser.add_argument('-g','--gcm',type=str,help=arg_help[3],default='')
+    parser.add_argument('-g','--gcm',type=str,help=arg_help[3],default='Can-ES-M2')
     parser.add_argument('-np','--nprocs',type=int,help=arg_help[4],default=20)
     parser.add_argument('-in','--infiles',type=str,help=arg_help[5],default='/scratch/hydroclimate/vic/colorado/results/PROCESSGCM/bal_*')
     parser.add_argument('-out','--outdir',type=str,help=arg_help[6],default='/scratch/hydroclimate/vic/colorado/results/testing')
@@ -133,17 +145,22 @@ if (__name__ == '__main__'):
 
     # Init global timer and generate control files
     print('Initializing...')
+    print("GCM: {}\nSEED: {}\nOUT_DIR: {}\nNPROCS: {}\nYEAR_RANGE: ({},{})\n".format(
+        args.gcm,SEED,args.outdir,args.nprocs,args.year_begin,args.year_end))
+
     t0 = time.perf_counter()
-    control_files = create_control_files(args.nprocs,args.gcm,[args.year_begin,args.year_end])
+    control_files = create_control_files(args)
 
     # Run all generated control files among NPROC files
     print('Generating processor pool...')
-    pool = Pool(processes=args.nprocs)
+    pool = multiprocessing.Pool(processes=args.nprocs)
     pool.map(run_config, control_files)
     print('Done.')
 
+    # Cleanup
+    for file in control_files:
+        os.remove(file)
+
     # Close global timer and print out
     t1 = time.perf_counter()
-    print("Total runtime: {} hrs".format(round((t1-t0)/60./60.,3))
-
-
+    print("Total runtime: {} hrs".format(round((t1-t0)/60./60.,3)))
---
+++ run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# ====================================== #
+#                                        #
+#  VIC to NETCDF BATCH CONVERSION SCRIPT #
+#           ----------------             #
+#  This should be called instead of the  #
+#  Python script when trying to do a     #
+#  large-scale conversion of all VIC     #
+#  files.                                #
+#           ----------------             #
+#  TO USE:                               #
+#    1. Change the variables to fit your #
+#       specific needs.                  #
+#    2. MAKE SURE to change the year     #
+#       range! Use the same script on 4  #
+#       different machines, with years:  #
+#          machine_1: (1950 1990)        #
+#          machine_2: (1991 2030)        #
+#          machine_3: (2031 2070)        #
+#          machine_4: (2071 2098)        #
+#  Assuming the global range (1950 2098) #
+# ====================================== #
+
+#source activate tonic2
+
+# How many processors should run?
+nprocs=20
+
+# Directory that stores infiles.
+# Due to the wildcard, this should be enclosed in a string.
+in_dir="/scratch/hydroclimate/vic/colorado/results/PROCESSGCM/bal_*"
+
+# The year range should be no more than a 40 year span.
+# Use other machines to do the others.
+declare -a year_range=(1950 1990)
+
+# Define the GCMs to iterate over.
+declare -a gcm_list=("CSIRO-Mk3-6-0" "GFDL-ESM2G" "GFDL-ESM2M" "HadGEM2-CC365"
+                     "HadGEM2-ES365" "IPSL-CM5A-LR" "IPSL-CM5B-LR" "MIROC-ESM-CHEM"
+                     "MIROC-ESM" "MPI-ESM-LR" "NorESM1-M")
+
+
+# Iterate over each GCM and run.
+for gcm in "${gcm_list[@]}" ; do
+
+    # Generate a GCM-dependent prefix
+    prefix="vic_$gcm" 
+
+    # Store files in GCM-named directory
+    out_dir="/scratch/hydroclimate/vic/colorado/results/testing/$gcm"
+
+    # Run Python script
+    python driver.py -np $nprocs -g $gcm -y0 ${year_range[0]} -y1 ${year_range[1]} -in $in_dir -out $out_dir -p $prefix
+done
